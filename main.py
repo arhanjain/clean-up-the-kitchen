01c58dc2cdb35047fc77f68e05b1bf3ec10d5aa1
Raymond Yu <ryu5@uw.edu>
2024-07-22 17:40:19 -0700

    added grasp sampling to main

--- main.py
+++ main.py
@@ -42,8 +42,54 @@ from omni.isaac.lab.envs import ManagerBasedRLEnvCfg
 
 from planner import MotionPlanner
 from cleandakitchen import TestWrapper
-import cleandakitchen
 
+# These are all things I tried to make curobo grasp correctly: 
+
+# def pos_and_quat_from_matrix(transform_mat):
+#     pos = transform_mat[:3, -1]
+#     quat = quat_from_matrix(transform_mat[:3, :3])
+#     return pos, quat
+
+def pos_and_quat_from_matrix(transform_mat):
+    transform_mat = np.array(transform_mat)  # Convert the list of lists to a numpy array
+    pos = torch.tensor(transform_mat[:3, -1], dtype=torch.float32)
+    quat = torch.tensor(transform_mat[:3, :3], dtype=torch.float32)  # Convert to tensor for further processing
+    quat_res = quat_from_matrix(quat)
+      # Quaternion representing a 180-degree rotation around the x-axis
+    quat_180_x = torch.tensor([0.0, 1.0, 0.0, 0.0], dtype=torch.float32)
+    # Rotate the quaternion by 180 degrees
+    rotated_quat = quaternion_multiply(quat_res, quat_180_x)
+    return quat, rotated_quat
+
+def quaternion_multiply(q, r):
+    w1, x1, y1, z1 = q
+    w2, x2, y2, z2 = r
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+    return torch.tensor([w, x, y, z], dtype=torch.float32)
+# def quaternion_multiply(q, r):
+#     w1, x1, y1, z1 = q
+#     w2, x2, y2, z2 = r
+#     w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+#     x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+#     y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+#     z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+#     return torch.tensor([w, x, y, z], dtype=torch.float32)
+
+# def pos_and_quat_from_matrix(transform_mat):
+#     pos = torch.tensor(transform_mat[:3, -1], dtype=torch.float32)
+#     quat = torch.tensor(transform_mat[:3, :3], dtype=torch.float32)  # Convert to tensor for further processing
+#     quat_res = quat_from_matrix(quat)
+
+#     # Quaternion representing a 180-degree rotation around the x-axis
+#     quat_180_x = torch.tensor([0.0, 1.0, 0.0, 0.0], dtype=torch.float32)
+
+#     # Rotate the quaternion by 180 degrees
+#     rotated_quat = quaternion_multiply(quat_res, quat_180_x)
+
+#     return pos, rotated_quat
 
 def main():
     # create environment configuration
@@ -66,29 +112,54 @@ def main():
     while simulation_app.is_running():
         # run everything in inference mode
         joint_pos, joint_vel, joint_names = env.get_joint_info()
-        # goal = torch.tensor([0.5, -0.5, 0.7, 0.707, 0, 0.707, 0]).repeat(env.num_envs, 1).to(env.unwrapped.device)
-        goal = env.command_manager.get_command("object_pose")
-        plan, success = planner.plan(joint_pos, joint_vel, joint_names, goal, mode="ee_pose")
-        with torch.inference_mode():
-            if not success:
-                env.reset()
-                continue
-            plan = planner.pad_and_format(plan)
-            for pose in plan:
-                # joint_pos, joint_vel, joint_names = env.get_joint_info()
-                # cur_pose = planner.fk(joint_pos)
-                # print(f"EE Pose: {cur_pose.ee_position}")
-                # print(f"Desired Pose: {pose}")
-                gripper = torch.ones(env.num_envs, 1).to(0)
-                # print(f"pose: {pose.shape} gripper: {gripper.shape}")
-                try:
-                    # pose = torch.unique(pose1, dim=0) # work around for dups in motion plan
-                    action = torch.cat((pose, gripper), dim=1)
-                except:
-                    breakpoint()
-                    plan = planner.plan(joint_pos, joint_vel, joint_names, goal, mode="ee_pose")
-                env.step(action)
-                im = env.get_camera_data()
+        meta_data, rgb, seg, depth = env.get_camera_data()
+        # Not always consistent, hit or miss, currently using 25 inference passes from M2T2 config
+        cfg = OmegaConf.load("/home/jacob/projects/clean-up-the-kitchen/M2T2/config.yaml")
+        data, outputs = load_and_predict(cfg, meta_data, rgb, depth, seg)
+
+        # Visualize through meshcat-viewer
+        visualize(cfg, data, outputs)
+        # Gives transformation matricies for each object
+        grasps = np.array(outputs['grasps'])
+        grasp_conf = torch.tensor(outputs['grasp_confidence'], dtype = torch.float32)
+        grasp_conf = outputs['grasp_confidence']
+        # Currently only support for grasping one type of object in one environment
+
+        if grasps.size > 0 and len(grasps[0]) == len(grasp_conf):
+            sorted_indices = sorted(range(len(grasp_conf)), key=lambda i: grasp_conf[i], reverse=True)
+            grasps = grasps[0][sorted_indices]
+            grasp_conf = [grasp_conf[i] for i in sorted_indices]
+        else:
+            print("Length of grasps and grasp_confidence do not match.")
+        sorted_indices = torch.argsort(grasp_conf, descending = True) 
+        grasps = grasps[sorted_indices]
+        success = False
+        for i in range(grasps[0].shape[0]):
+            best_grasp = grasps[0][i]
+            pos, quat = pos_and_quat_from_matrix(best_grasp)
+            goal = torch.cat([pos, quat], dim=0).unsqueeze(0).repeat(env.num_envs, 1).to(env.unwrapped.device)
+            plan, success = planner.plan(joint_pos, joint_vel, joint_names, goal, mode="ee_pose")
+            if success:
+                # print(f"Grasp {i} succeeded with confidence {grasp_conf[sorted_indices[i]]}")
+                # print(f"Grasp {i} succeeded with confidence {grasp_conf[i]}")
+                break
+        if success:
+            with torch.inference_mode():
+                if not success:
+                    env.reset()
+                    continue
+                plan = planner.pad_and_format(plan)
+                for pose in plan:
+                    gripper = torch.ones(env.num_envs, 1).to(0)
+                    try:
+                        action = torch.cat((pose, gripper), dim=1)
+                    except:
+                        breakpoint()
+                        plan = planner.plan(joint_pos, joint_vel, joint_names, goal, mode="ee_pose")
+                    env.step(action)
+        else:
+            print("No successful grasp found")
+        env.close()
 
             # env.reset()
     # close the simulator
